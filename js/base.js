36e9f0ac7343a6d0aea5cea1a9d55730ac8e79a9
AyakaKusakari <52470652+AyakaKusakari@users.noreply.github.com>
2020-03-15 05:39:09 +0900

    Update base.js

--- js/base.js
+++ js/base.js
@@ -8,16 +8,19 @@ jQuery(window).on('scroll', function ($) {
 });
 
 // SmoothScroll
-jQuery('a[href^="#"]').click(function () {
-    var header = 0;
-    var speed = 300;
-    var id = jQuery(this).attr('href');
-    var target = jQuery('#' == id ? 'html' : id);
-    var position = jQuery(target).offset().top - header;
+jQuery('a[href^="#"]').click(function() {
+    let header = jQuery("#header").height();
+    let speed = 300;
+    let id = jQuery(this).attr("href");
+    let target = jQuery("#" == id ? "html" : id);
+    let position = jQuery(target).offset().top - header;
+    if ("fixed" !== jQuery("#header").css("position")) {
+        position = jQuery(target).offset().top;
+    }
     if (0 > position) {
         position = 0;
     }
-    jQuery('html, body').animate(
+    jQuery("html, body").animate(
         {
             scrollTop: position
         },
